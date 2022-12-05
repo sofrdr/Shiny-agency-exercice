98c8e2379ed4d3f9cd3a6f7f8a66d998a4b08703
Sophie Roudier <sophie.roudier@hotmail.fr>
2022-12-05 12:34:41 +0100

    Importing survey data from API

--- src/pages/Survey/Survey.jsx
+++ src/pages/Survey/Survey.jsx
@@ -1,22 +1,68 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+import styled from "styled-components";
+import colors from "../../utils/style/color";
 import { useParams, Link } from "react-router-dom";
 
 const Survey = () => {
   let { questionNumber } = useParams();
+  const questionNumberInt = parseInt(questionNumber);
+
+  const [surveyData, setSurveyData] = useState({});
+
+  const SurveyContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `;
+
+  const QuestionTitle = styled.h2`
+    text-decoration: underline;
+    text-decoration-color: ${colors.primary};
+  `;
+
+  const QuestionContent = styled.span`
+    margin: 30px;
+  `;
+
+  const LinkWrapper = styled.div`
+    padding-top: 30px;
+    & a {
+      color: black;
+    }
+    & a:first-of-type {
+      margin-right: 20px;
+    }
+  `;
+
+  async function fetchData() {
+    try {
+      const response = await fetch("http://localhost:8000/survey");
+      const data = await response.json();
+      setSurveyData(data.surveyData);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
-    <div>
-      <h1>Questionnaire</h1>
-      <h2>Question {questionNumber}</h2>
-      {questionNumber !== "1" && (
-        <Link to={`/survey/${Number(questionNumber) - 1}`}>Précédent</Link>
-      )}
-      {questionNumber === "10" ? (
-        <Link to="/survey/results">Résultats</Link>
-      ) : (
-        <Link to={`/survey/${Number(questionNumber) + 1}`}>Suivant</Link>
-      )}
-    </div>
+    <SurveyContainer>
+      <QuestionTitle>Question {questionNumber}</QuestionTitle>
+      <QuestionContent>{surveyData[questionNumber]}</QuestionContent>
+      <LinkWrapper>
+        {questionNumberInt > 1 && (
+          <Link to={`/survey/${questionNumberInt - 1}`}>Précédent</Link>
+        )}
+        {surveyData[questionNumberInt + 1] ? (
+          <Link to={`/survey/${questionNumberInt + 1}`}>Suivant</Link>
+        ) : (
+          <Link to="/survey/results">Résultats</Link>
+        )}
+      </LinkWrapper>
+    </SurveyContainer>
   );
 };
 
